c8b979d73571598bb4e4721356f1a19ac46a8ee4
jjvalletta <jjvalletta@gmail.com>
2018-04-15 11:03:26 +0100

    Add generic PCA plot func that takes multiple organs as input

--- Analysis/util.py
+++ Analysis/util.py
@@ -18,9 +18,11 @@ import pandas as pd # version 0.20.1
 import numpy as np # version 1.12.1
 import matplotlib.pyplot as plt # version 2.0.2
 import os
+from functools import reduce
+from collections import OrderedDict # important for consistent legend
 # "Local" libraries
 import input_output as io
-from config import TOP_RANKED, COL, LWD
+from config import TOP_RANKED, COL, LWD, STRAINS, ORGANS
 
 #*************************************************************************************************#
 # Functions
@@ -66,55 +68,55 @@ def top_ranked_genes(organ, strain, path, topRanked=TOP_RANKED, IDType='ProbeID'
     return set(data.iloc[:topRanked][IDType])
 
 #*************************************************************************************************#
-def pca_plot(organ, strain, path, bLegend=False):
+def _pca_plot(obj, embedding, markerList=None, colList=None, legDict=None, bLegend=False):
     """
-    Performs and plots PCA for organ and strain. The plot is saved to path['Misc']
+    Low-level PCA plot routine. 
+    
+    Note: the ORDER of markerList and colList HAS to match the order of the 
+    matrix used when creating the PCA object
          
     Arguments
     =========
-    organ - Blood/Spleen
-    strain - AS/CB
-    path - a structured dictionary of paths returned by config
+    obj - sklearn.decomposition.pca.PCA object
+    embedding - PCA(n_components=2).fit(y)
+    markerList - list of marker strings ['o', '^', etc..] ORDER important
+    colList - list of colours ['r', 'g', 'g', ...] ORDER important
+    legDict - dictionary of legend name and colour e.g ['Day1': 'black, 'Day2': 'blue']
     bLegend - a Boolean whether to show/hide legend
   
     Returns
     =========
-    None - PCA plot and PCA weights figure is saved as a .pdf to path['Misc']
+    (hWt, hPCA) - figure handles to weights and PCA figure
     
     """ 
-    # Read data
-    # Note: PCA for Log2FC or Raw is the same as we're only subtracting a constant
-    data = pd.read_csv(os.path.join(path['Data'], organ + strain + '.csv'))
-    #probeID = top_ranked_genes(organ, strain, path) # if we only want to consider TOP_RANKED
-    #data = data[data['ProbeID'].isin(probeID)] # subset data
-    x = np.floor(data.filter(regex='\.').columns.values.astype('float')).astype('int')
-    y = data.filter(regex='\.').as_matrix().T # samples/days=rows; features/genes=columns
+    # Create markers and cols if None
+    if markerList is None:
+        markerList = ['o']*obj.n_samples_
+    if colList is None:
+        colList = ['grey']*obj.n_samples_
     
-    # Colour dictonary and col list
-    colDict = {}
-    for i, value in enumerate(np.unique(x)):
-        colDict[value] = COL['PCA'][i]
-    myCol = [colDict[value] for value in x]
-    
-    # Perform PCA        
-    pcaOut = PCA(n_components=2).fit(y)
-    PC = [np.round(pcaOut.explained_variance_ratio_[i]*100) for i in range(pcaOut.n_components)]
-    embedding = pcaOut.transform(y)
+    # Extract explained variance and transformed variables 
+    PC = [np.round(obj.explained_variance_ratio_[i]*100) for i in range(obj.n_components)]
     
     # Plot weights
-    hFig, hAx = plt.subplots(2, sharex=True, figsize=(12, 12))
+    hWt, hAx = plt.subplots(2, sharex=True, figsize=(12, 12))
     for i in range(len(PC)):
-        hAx[i].plot(np.sort(pcaOut.components_[i, :]**2)[::-1]*100)
-        hAx[i].set_title('PC{}: {:.0f}%'.format(i, PC[i]))
-        hAx[i].set_ylabel('PCA Weight ($\sum$ weights = 100)')
+        hAx[i].plot(np.sort(obj.components_[i, :]**2)[::-1]*100)
+        hAx[i].set_title('PC{}: {:.0f}%'.format(i+1, PC[i]))
+        hAx[i].set_ylabel('PCA Weight ($\sum$ weights = 1)')
         hAx[i].set_xlabel('No. of genes')
-    io.save_pdf(os.path.join(path['Misc'], 'PCAWeights' + organ + strain + '.pdf'), hFig)
-    
+        hAx[i].axhline(y=0, color=COL['Zero'], linestyle='--', linewidth=LWD['M'])
+
+
     # Create PCA figure
-    hFig = plt.figure(figsize=(8, 8)); hAx = plt.gca()
-    hAx.scatter(embedding[:, 0], embedding[:, 1], linewidth=LWD['M'], edgecolor='black', 
-                c=myCol, s=300)
-    
+    # Note: marker cannot be a list so have to loop over unique markers (argh!)
+    hPCA = plt.figure(figsize=(8, 8)); hAx = plt.gca()
+    markers = np.array(markerList)
+    cols = np.array(colList)
+    for m in np.unique(markers):
+        hAx.scatter(embedding[markers==m, 0], embedding[markers==m, 1], linewidth=LWD['M'], 
+                    edgecolor='black', c=cols[markers==m], s=300, marker=m)
+
     # Set x/y limits
     xMin, yMin = np.min(embedding, axis=0)
     xMax, yMax = np.max(embedding, axis=0)
@@ -124,22 +126,82 @@ def pca_plot(organ, strain, path, bLegend=False):
     hAx.set_ylabel('PC2: {:.0f}%'.format(PC[1]), fontsize=24)
     plt.tick_params(labelsize=16) # change tixk font 
     plt.tight_layout()
-    #plt.axes().set_aspect('equal', 'datalim')
+    plt.axes().set_aspect('equal', 'datalim')
     
     # Add legend if wanted
-    if bLegend:
-        col = []
-        lab = []
-        for key, value in colDict.items():
-            lab.append('Day {}'.format(key))
-            col.append(value)
-        # 'Hack' to show legend
-        faff = [hAx.scatter([], [], edgecolor='black', c=c) for c in col]        
-        hAx.legend(faff, lab, fontsize=20, ncol=1, markerscale=2, 
+    if bLegend and legDict is not None:
+        faff = [] # 'Hack' to show legend 
+        for key, value in legDict.items():
+            faff.append(hAx.scatter([], [], edgecolor='black', c=value[0], marker=value[1]))
+        hAx.legend(faff, legDict.keys(), fontsize=20, ncol=1, markerscale=2, 
                    handlelength=0.2, borderpad=-0.4, loc=0, frameon=False)
+        
+    return hWt, hPCA
+
+#*************************************************************************************************#
+def pca_plot(organ, strain, path, topRanked=None, bLegend=False):
+    """
+    Performs and plots PCA for organ(s) and strain.
+         
+    Arguments
+    =========
+    organ - a list with one or two organs e.g ['Spleen'] or ['Blood', 'Spleen']
+    strain - single strain AS/CB (string)
+    path - a structured dictionary of paths returned by config
+    topRanked - how many genes to consider (int) or None to consider everything
+    bLegend - a Boolean whether to show/hide legend
+  
+    Returns
+    =========
+    (hWt, hPCA) - figure handles to weights and PCA figure
     
-    # Write to disk
-    io.save_pdf(os.path.join(path['Misc'], "PCA" + organ + strain + ".pdf"), hFig) 
+    """ 
+    # Assert inputs are ok
+    set(organ).issubset(ORGANS), '{} not a valid organ! Use {}'.format(organ, ORGANS)
+    assert strain in STRAINS, '{} not a valid strain! Use {}'.format(strain, STRAINS)
+    
+    # Set marker dictionary
+    marker = {'Bl': 'o', 'Sp': '^'}
+        
+    # Read data
+    df = [] # list of data frames
+    for org in organ:
+        temp = pd.read_csv(os.path.join(path['Data'], org + strain + '.csv'))
+        temp = temp.set_index('ProbeID')
+        temp = temp.drop(labels='Symbol', axis=1)
+        temp = temp.add_prefix(org[:2] + '_')
+        if topRanked is not None:
+            probeID = top_ranked_genes(org, strain, path)
+            temp = temp.loc[probeID]
+        df.append(temp)
+        
+    # Merge into single data frame
+    data = reduce(lambda left, right: pd.merge(left, right, left_index=True, right_index=True), df)
+    del(df)
+    y = data.filter(regex='\.').as_matrix().T # samples/days=rows; features/genes=columns
+    
+    # Perform PCA
+    obj = PCA(n_components=2).fit(y)
+    embedding = obj.transform(y)
+        
+    # Create marker + colour list; preserve ORDER
+    # And legend dictionary
+    legDict = OrderedDict()
+    markerList = []
+    colList = []
+    for col in data.columns:
+        # Marker+col list
+        org = col.split('_')[0]
+        day = int(col.split('_')[1].split('.')[0])
+        markerList.append(marker[org])
+        colList.append(COL['PCA'][int(day/2)])
+        # Legend 
+        x = col.split('.')[0] # ignore replicate
+        if x not in legDict:
+            legDict[x] = [COL['PCA'][int(day/2)], marker[org]]        
+            
+    # Return figure handles
+    return _pca_plot(obj, embedding, markerList, colList, legDict, bLegend)
 
 #*************************************************************************************************#
 def cluster_membership_boxplot(organ, strain, path):
@@ -171,7 +233,7 @@ def cluster_membership_boxplot(organ, strain, path):
         clustName.append(data['Cluster'][label])
 
     # Create boxplot
-    hFig = plt.figure(figsize=(16/1.2, 9/2))    
+    hFig = plt.figure(figsize=(16, 5))    
     hAx = hFig.gca()    
     hAx.boxplot(membership, labels=clustName, patch_artist=True, notch=True,
                 boxprops={"facecolor": COL[organ]},
